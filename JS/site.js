e4d621a048b1cb730b9fd8bdda2a5c3efc805f03
makinahmet <makinahmet@gmail.com>
2024-03-15 13:15:02 +0300

    Yeni menü sitili eklendi yazılar düzeltildi.

--- JS/site.js
+++ JS/site.js
@@ -1,22 +1,32 @@
 const leftMenu =
-'<b class="text-white"><div class="vertical-navbar bg-blue-4 p-10 radius-5 mr-10">'+
-        '<a class="text-black" href="index.html">Giriş</a>'+
-        '<a class="text-black" href="isimlendirmeler.html">isimlendirmeler</a>'+
-        '<a class="text-black" href="yorumlar.html">Yorumlar</a>'+
-        '<a class="text-black" href="metotlar.html">Metotlar</a>'+
-    '</div></b>';
+'<div class="hamnav">'+
+    '<!-- (B) THE HAMBURGER -->'+
+    '<label for="verticalHamburger" class="fs-l">☰</label>'+
+    '<input type="checkbox" id="verticalHamburger" class="hamChekbox"/>'+
+
+    '<!-- (C) MENU ITEMS -->'+
+    '<div class="hamitems">'+
+        '<div class="row text-white vertical-navbar bg-blue-4 mt-10 p-10 radius-5">'+
+            '<b>KONULAR</b>'+
+            '<a class="text-white" href="index.html">Giriş</a>'+
+            '<a class="text-white" href="isimlendirmeler.html">isimlendirmeler</a>'+
+            '<a class="text-white" href="yorumlar.html">Yorumlar</a>'+
+            '<a class="text-white" href="metotlar.html">Metotlar</a>'+
+        '</div>'+
+    '</div>'+
+'</div>';
 
 document.getElementById("leftMenu").innerHTML = leftMenu;
 
 const navbar = 
 '<div class="col-2 justify-content-center vertical-align-center"><img class="logo" src="images/abmLogo.png"></div>'+
-'<div class="col-10 justify-content-center vertical-align-center text-white"><span class="fs-xxl">Etkili Kod Yazma Teknikleri</span></div>';
+'<div class="col-10 justify-content-center vertical-align-center text-white"><span class="fs-m">Etkili Kod Yazma Teknikleri</span></div>';
 
 document.getElementById("navbar").innerHTML = navbar;
 
 const footer =
 '<div class="col-12 justify-content-center vertical-align-center vh-5 bg-blue-4 radius-5">'+
-'<span class="text-white"> <a class="no-decoration" href="https://makinahmet.github.io/abmcss.github.io/">ABMCSS</a> kullanılarak yazılmıştır. </span>'+
+'<span class="text-white"> <a class="no-decoration text-white" href="https://makinahmet.github.io/abmcss.github.io/">ABMCSS</a> kullanılarak yazılmıştır. </span>'+
 '</div>';
 
 document.getElementById("footer").innerHTML = footer;
